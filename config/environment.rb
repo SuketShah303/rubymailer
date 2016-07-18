6a72cfb6ad25bd8ba90afc6300e519299dc0545e
mehta-jay <mehta_jay@network.elanco.com>
2016-07-18 23:04:30 +0530

    Update environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -8,8 +8,8 @@ Rails.application.initialize!
 ActionMailer::Base.smtp_settings = {
   :port           => 587,
   :address        => 'smtp.mailgun.org',
-  :user_name      => 'postmaster@sensationnel-croissant-56079.herokuapp.com',
-  :password       => '87f2a340a9f6e24d82601b64b3d3f545',
+  :user_name      => 'postmaster@sandboxf4b444403f6947ad873a2d5f489bfb52.mailgun.org',
+  :password       => 'cb2e7740cc9845221f2cd6d8351b9b69',
   :domain         => 'sensationnel-croissant-56079.herokuapp.com',
   :authentication => :plain,
 }
