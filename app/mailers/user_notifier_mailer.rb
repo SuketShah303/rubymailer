d57d6afbfd81c9b7dd67e8d28e41887dedaf57c4
mehta-jay <mehta_jay@network.elanco.com>
2016-07-18 23:28:08 +0530

    Update user_notifier_mailer.rb

--- app/mailers/user_notifier_mailer.rb
+++ app/mailers/user_notifier_mailer.rb
@@ -4,8 +4,12 @@ class UserNotifierMailer < ApplicationMailer
 
   # send a signup email to the user, pass in the user object that   contains the user's email address
   def send_signup_email(user)
-    @user = user
-    mail( :to => @user.email,
-    :subject => 'Thanks for signing up for our amazing app' )
+    
+    RestClient.post "https://api:aeabaf758234ab2b56c8cdd8cd662a25"\
+  "@api.mailgun.net/v3/sensationnel-croissant-56079.herokuapp.com/messages",
+  :from => "Excited User <mailgun@sensationnel-croissant-56079.herokuapp.com>",
+  :to => "jaymehta.ce@gmail.com, jay@sensationnel-croissant-56079.herokuapp.com",
+  :subject => "Hello",
+  :text => "Testing some Mailgun awesomness!"
   end
 end
