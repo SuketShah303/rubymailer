238360d551279ae4934caa32243800ec94bb1abe
SuketShah303 <shah_suket@network.elanco.com>
2016-07-25 20:10:47 +0530

    Update user_notifier_mailer_test.rb

--- test/mailers/user_notifier_mailer_test.rb
+++ test/mailers/user_notifier_mailer_test.rb
@@ -4,4 +4,8 @@ class UserNotifierMailerTest < ActionMailer::TestCase
   # test "the truth" do
   #   assert true
   # end
+  def demo_mail(user)
+            @user=user
+ 			mail to: @user.email, subject:"Registration confirmation" 
+    end
 end
